ca5654dbfa0b010ce90f556f0f4491d7209ef1d3
screamcha <scream1234540@gmail.com>
2019-12-16 15:51:43 +0300

    move api mock to a separate file

--- src/tests/services/Jira.test.ts
+++ src/tests/services/Jira.test.ts
@@ -1,84 +1,13 @@
-import nock from 'nock'
-import HttpCodes from 'http-status-codes'
-import { format } from 'date-fns'
-
+import { mockApi, testAuthToken, resultIssues } from '../mocks/jiraApi'
 import { jiraService } from '../../services/Jira'
-import { JIRA_METRICS_API_URL, DATE_FORMAT } from '../../constants'
 
 import { EIssueType, IIssueParameters } from '../../models/Jira.model'
-import { IJiraApiSearchResult } from '../../models/JiraApi.model'
 import { Issue } from '../../models/Issue'
 import { IUser } from '../../models/User'
 
 describe('Jira Service', () => {
-  const testAuthToken = 'test-auth-key'
-  const testIssueParameters = {
-    jql: `
-      issuetype in ('${EIssueType.Bug}')
-        AND status in (Dev-complete, Discarded)
-        AND
-          (
-            (updated >= ${format(new Date('2019-01-01'), DATE_FORMAT)}
-            AND updated <= ${format(new Date('2019-12-31'), DATE_FORMAT)})
-          OR
-            (created >= ${format(new Date('2019-01-01'), DATE_FORMAT)}
-            AND created <= ${format(new Date('2019-12-31'), DATE_FORMAT)})
-          )
-        AND
-          assignee = user1
-    `,
-    expand: ['changelog'],
-    fields: ['issuelinks', 'issuetype'],
-  }
-  const resultIssues: IJiraApiSearchResult = {
-    issues: [
-      {
-        key: 'issue-1',
-        fields: {
-          issuetype: {
-            id: EIssueType.Dev,
-          },
-        },
-        changelog: {
-          histories: [
-            {
-              id: '111',
-              author: {
-                displayName: 'Thrall',
-                key: 'thrall-horde',
-                emailAddress: 'thrall@azeroth.com',
-              },
-              created: '2019-01-03',
-              items: [
-                {
-                  field: 'timespent',
-                  from: null,
-                  to: '10000',
-                }
-              ],
-            }
-          ],
-        },
-      }
-    ],
-  }
-
   beforeAll(() => {
-    nock(JIRA_METRICS_API_URL)
-      .get('/myself')
-      .reply(function () {
-        if (this.req.headers.authorization !== `Bearer ${testAuthToken}`) {
-          return [HttpCodes.UNAUTHORIZED, null]
-        }
-
-        return [HttpCodes.ACCEPTED, {
-          emailAddress: 'test@test.com',
-          displayName: 'Varok Saurfang',
-          key: 'saurfang',
-        }]
-      })
-      .post('/search', testIssueParameters)
-      .reply(200, resultIssues)
+    mockApi()
   })
 
   describe('currentUser method', () => {
@@ -110,8 +39,8 @@ describe('Jira Service', () => {
   describe('getIssues method', () => {
     it('returns array of issues for given parameters', async () => {
       const testParameters: IIssueParameters = {
-        startDate: new Date(2019, 0, 1),
-        endDate: new Date(2019, 11, 31),
+        startDate: new Date('2019-01-01'),
+        endDate: new Date('2019-12-31'),
         issueTypes: [EIssueType.Bug],
         userKey: 'user1',
       }
